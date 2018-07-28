30b20c033d6f8b1fb8bdf39056a4acbd348f44d8
Shubhashish Verma <svashish305@gmail.com>
2018-07-28 17:17:48 +0530

    Add files via upload

---
+++ notes.js
@@ -0,0 +1,67 @@
+// console.log('Starting notes.js');
+
+const fs = require('fs');
+var fetchNotes = () => {
+  try {
+    var noteString = fs.readFileSync('notes-data.json');
+    return JSON.parse(noteString);
+  } catch (e) {
+    return [];
+  }
+};
+
+var saveNotes = (notes) => {
+  fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+};
+
+var addNote = (title, body) => {
+  var notes = fetchNotes();
+  var note = {
+    title,
+    body
+  };
+
+var duplicateNotes = notes.filter((note) => note.title === title);
+
+  if (duplicateNotes.length === 0) {
+    notes.push(note);
+    saveNotes(notes);
+    return note;
+  }
+};
+
+var getAll = () => {
+  return fetchNotes();
+};
+
+var getNote = (title) => {
+  var notes = fetchNotes();
+  var filteredNotes = notes.filter((note) => note.title === title);
+  return filteredNotes[0];
+};
+
+var removeNote = (title) => {
+  // fetch notes
+  var notes = fetchNotes();
+  // filter notes, removing the one with title of argument
+  var filteredNotes = notes.filter((note) => note.title !== title);
+  //save new notes array
+  saveNotes(filteredNotes);
+  return notes.length !== filteredNotes.length;
+};
+
+var logNote = (note) => {
+  //debugger; // Break on this line and use replto putput note
+  // Use read command with --title
+  console.log('--');
+  console.log(`Title:  ${note.title}`);
+  console.log(`Body:  ${note.body}`);
+};
+
+module.exports = {
+  addNote,
+  getAll,
+  getNote,
+  removeNote,
+  logNote
+};
